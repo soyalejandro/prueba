21098bc33e951f21ac11a961f4866003359e84e3
Alejandro <alejandro98_3@hotmail.com>
2019-05-07 20:13:44 -0500

    Prueba

---
+++ funciones.js
@@ -0,0 +1,19 @@
+function mensaje(){
+	alert("Hola Mundo");
+}
+
+function nombre1(){
+	alert("Alejandro");
+}
+
+function nombre2(){
+	alert("Alejandro");
+}
+
+function ap_paterno(){
+	alert("Alvarez");
+}
+
+function ap_materno(){
+	alert("Alvarez");
+}
